0cf6cccf88aeef2f7d80f4fd1d2d2153ed984c8a
dekannabesetale <88384604+dekannabesetale@users.noreply.github.com>
2025-07-08 18:30:49 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import HomePage from './HomePage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <HomePage />
+  </React.StrictMode>,
+)
